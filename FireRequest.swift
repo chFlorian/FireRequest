9eb2bc5e45bfddec25f25ca6c61c0993db174ec4
florian schweizer <florian.schweizer@tum.de>
2021-07-15 10:39:04 +0200

    Add more predicates

--- FireRequest.swift
+++ FireRequest.swift
@@ -8,17 +8,33 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import SwiftUI
 
+enum FIRPredicate {
+    case isEqualTo(_ lhs: String, rhs: Any)
+    
+    case isIn(_ lhs: String, rhs: [Any])
+    case isNotIn(_ lhs: String, rhs: [Any])
+    
+    case arrayContains(_ lhs: String, rhs: Any)
+    case arrayContainsAny(_ lhs: String, rhs: [Any])
+    
+    case isLessThan(_ lhs: String, rhs: Any)
+    case isGreaterThan(_ lhs: String, rhs: Any)
+    
+    case isLessThanOrEqualTo(_ lhs: String, rhs: Any)
+    case isGreaterThanOrEqualTo(_ lhs: String, rhs: Any)
+}
+
 @propertyWrapper
 public class FireRequest<T: Decodable> {
     @Published public var wrappedValue: [T] = []
     
     private let store = Firestore.firestore()
     
-    init(_ collection: String, _ predicates: [FRPredicate] = []) {
+    init(_ collection: String, _ predicates: [FIRPredicate] = []) {
         load(from: collection, withPredicates: predicates)
     }
     
-    private func load(from collection: String, withPredicates predicates: [FRPredicate] = []) {
+    private func load(from collection: String, withPredicates predicates: [FIRPredicate] = []) {
         let reference = store.collection(collection)
         
         if predicates.isEmpty {
@@ -37,10 +53,58 @@ public class FireRequest<T: Decodable> {
             for predicate in predicates {
                 switch predicate {
                     case .isEqualTo(let lhs, let rhs):
-                        if let tampQuery = query {
-                            query = tampQuery.whereField(lhs, isEqualTo: rhs)
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, isEqualTo: rhs)
                         } else {
                             query = reference.whereField(lhs, isEqualTo: rhs)
+                        }
+                    case .isIn(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, in: rhs)
+                        } else {
+                            query = reference.whereField(lhs, in: rhs)
+                        }
+                    case .isNotIn(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, notIn: rhs)
+                        } else {
+                            query = reference.whereField(lhs, notIn: rhs)
+                        }
+                    case .arrayContains(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, arrayContains: rhs)
+                        } else {
+                            query = reference.whereField(lhs, arrayContains: rhs)
+                        }
+                    case .arrayContainsAny(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, arrayContainsAny: rhs)
+                        } else {
+                            query = reference.whereField(lhs, arrayContainsAny: rhs)
+                        }
+                    case .isLessThan(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, isLessThan: rhs)
+                        } else {
+                            query = reference.whereField(lhs, isLessThan: rhs)
+                        }
+                    case .isGreaterThan(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, isGreaterThan: rhs)
+                        } else {
+                            query = reference.whereField(lhs, isGreaterThan: rhs)
+                        }
+                    case .isLessThanOrEqualTo(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, isLessThanOrEqualTo: rhs)
+                        } else {
+                            query = reference.whereField(lhs, isLessThanOrEqualTo: rhs)
+                        }
+                    case .isGreaterThanOrEqualTo(let lhs, let rhs):
+                        if let tempQuery = query {
+                            query = tempQuery.whereField(lhs, isGreaterThanOrEqualTo: rhs)
+                        } else {
+                            query = reference.whereField(lhs, isGreaterThanOrEqualTo: rhs)
                         }
                 }
             }
