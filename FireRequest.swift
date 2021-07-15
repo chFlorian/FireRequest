bec7a0107fcfce225ef8625227c75b714ae0f3d7
florian schweizer <florian.schweizer@tum.de>
2021-07-15 10:02:45 +0200

    Add predicates

--- FireRequest.swift
+++ FireRequest.swift
@@ -1,3 +1,9 @@
+//
+//  FireRequest
+//
+//  Created by Florian Schweizer on 12.07.21.
+//
+
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 import SwiftUI
@@ -7,26 +13,48 @@ public class FireRequest<T: Decodable> {
     @Published public var wrappedValue: [T] = []
     
     private let store = Firestore.firestore()
-    private let collection: String
     
-    init(_ collection: String) {
-        self.collection = collection
-        
-        load()
+    init(_ collection: String, _ predicates: [FRPredicate] = []) {
+        load(from: collection, withPredicates: predicates)
     }
     
-    private func load() {
-        store
-            .collection(collection)
-            .getDocuments { snapshot, error in
-                guard error == nil,
-                      let snapshot = snapshot else {
-                      return
-                  }
-                
-                self.wrappedValue = snapshot.documents.compactMap { document in
-                    try? document.data(as: T.self)
+    private func load(from collection: String, withPredicates predicates: [FRPredicate] = []) {
+        let reference = store.collection(collection)
+        
+        if predicates.isEmpty {
+            reference
+                .getDocuments { snapshot, error in
+                    guard error == nil, let snapshot = snapshot else {
+                        return
+                    }
+                    
+                    self.wrappedValue = snapshot.documents.compactMap { document in
+                        try? document.data(as: T.self)
+                    }
+                }
+        } else {
+            var query: Query? = nil
+            for predicate in predicates {
+                switch predicate {
+                    case .isEqualTo(let lhs, let rhs):
+                        if let tampQuery = query {
+                            query = tampQuery.whereField(lhs, isEqualTo: rhs)
+                        } else {
+                            query = reference.whereField(lhs, isEqualTo: rhs)
+                        }
                 }
             }
+            
+            query?
+                .getDocuments { snapshot, error in
+                    guard error == nil, let snapshot = snapshot else {
+                        return
+                    }
+                    
+                    self.wrappedValue = snapshot.documents.compactMap { document in
+                        try? document.data(as: T.self)
+                    }
+                }
+        }
     }
 }
